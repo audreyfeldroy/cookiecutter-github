0ed1cc240fd3da7c24b55622c7343c0580daf51a
Audrey Roy <audreyr@gmail.com>
2013-09-01 18:26:12 +0200

    Prep for next steps, when we have access to context.

--- {{cookiecutter.repo_name}}/hooks/post_gen_project.py
+++ {{cookiecutter.repo_name}}/hooks/post_gen_project.py
@@ -3,6 +3,7 @@
 from __future__ import print_function
 import getpass
 import json
+import os
 import sys
 
 import requests
@@ -12,11 +13,16 @@ print('post generation hook')
 f = open('python_post.txt', 'w')
 f.close()
 
+print('Enter the following info to authenticate with GitHub and create a repo.')
+
+# TODO: use cookiecutter['github_username'] instead
 sys.stdout.write('GitHub username? ')
 user = input()
+
 password = getpass.getpass()
 
 payload = {'name': 'test-repo'}
+
 r = requests.post(
         url='https://api.github.com/user/repos',
         data=json.dumps(payload),
